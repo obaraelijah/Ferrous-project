ba93d3a6e4eb86a6839459327d604f95349bb828
obaraelijah <elijahobara357@gmail.com>
2024-08-16 13:02:33 +0300

    feat: Added dummy login view

--- ferrous_frontend/src/views/login.tsx
+++ ferrous_frontend/src/views/login.tsx
@@ -1,16 +1,60 @@
 import React from "react";
+import { Api } from "../api/api";
+import { toast, ToastContentProps } from "react-toastify";
+
+import "../styling/login.css";
+import Input from "../components/input";
 
 type LoginProps = {};
-type LoginState = {};
+type LoginState = {
+    username: string;
+    password: string;
+};
 
 export default class Login extends React.Component<LoginProps, LoginState> {
     constructor(props: LoginProps) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            username: "",
+            password: "",
+        };
+
+        this.performLogin = this.performLogin.bind(this);
+    }
+
+    async performLogin(e: React.FormEvent) {
+        e.preventDefault();
+
+        (await Api.auth.login(this.state.username, this.state.password)).match(
+            async (_: any) => toast.success("Authenticated successfully"),
+            (err: { message: string }) => toast.error(err.message)
+        );
     }
 
     render() {
-        return <div></div>;
+        return (
+            <>
+                <form onSubmit={this.performLogin}>
+                    <div>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            value={this.state.username}
+                            onChange={(e) => this.setState({ username: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            value={this.state.password}
+                            onChange={(e) => this.setState({ password: e.target.value })}
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </>
+        );
     }
 }
