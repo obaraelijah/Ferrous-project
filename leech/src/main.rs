dfb92a1cc62c20c84392f2e7fe761b5f7caf079c
obaraelijah <elijahobara357@gmail.com>
2024-08-26 16:26:33 +0300

    feat: leech workers main config

--- leech/src/main.rs
+++ leech/src/main.rs
@@ -13,11 +13,80 @@
     allow(dead_code, unused_variables, unused_imports)
 )]
 
+use clap::{Parser, Subcommand};
+
+use crate::config::get_config;
+
+#[derive(Subcommand)]
+enum RunCommand {
+    PortScanner {
+        /// Valid IPv4 or IPv6 addresses or networks in CIDR notation
+        #[clap(required(true))]
+        targets: Vec<String>,
+        /// A single port, multiple, comma seperated ports or (inclusive) port ranges
+        ///
+        /// If no values are supplied, 1-65535 is used as default
+        #[clap(short = 'p')]
+        ports: Vec<String>,
+        /// Valid IPv4 or IPv6 addresses or networks in CIDR notation
+        #[clap(long)]
+        exclude: Vec<String>,
+        /// The time to wait until a connection is considered failed.
+        ///
+        /// The timeout is specified in milliseconds.
+        #[clap(long)]
+        #[clap(default_value_t = 1000)]
+        timeout: u16,
+        /// The concurrent task limit
+        #[clap(long)]
+        #[clap(default_value_t = NonZeroUsize::new(1000).unwrap())]
+        concurrent_limit: NonZeroUsize,
+        /// The number of times the connection should be retried if it failed.
+        #[clap(long)]
+        #[clap(default_value_t = 6)]
+        max_retries: u8,
+        /// The interval that should be wait between retries on a port.
+        ///
+        /// The interval is specified in milliseconds.
+        #[clap(long)]
+        #[clap(default_value_t = 100)]
+        retry_interval: u16,
+        /// Skips the initial ping check.
+        ///
+        /// All hosts are assumed to be reachable.
+        #[clap(long)]
+        #[clap(default_value_t = false)]
+        skip_ping_check: bool,
+    },
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Server,
+    Execute {
+        #[clap(subcommand)]
+        command: RunCommand,
+    },
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(long = "config-path")]
+    #[clap(help = "Specify an alternative path to the config file")]
+    #[clap(default_value_t = String::from("/etc/leech/config.toml"))]
+    config_path: String,
+
+    #[clap(subcommand)]
+    commands: Command,
+}
+
 pub mod config;
 pub mod modules;
 
 #[rorm::rorm_main]
 #[tokio::main]
-async fn main() {
-    println!("Ferrous, project!");
+async fn main() -> Result<(), String> {
+    let cli = Cli::parse();
+
+    let _config = get_config(&cli.config_path).map_err(|e| e.to_string())?;
 }
