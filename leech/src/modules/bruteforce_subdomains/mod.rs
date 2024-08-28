6ccfac2d923fddd64513211f08fabd7da3d51da5
obaraelijah <elijahobara357@gmail.com>
2024-08-28 17:08:54 +0300

    docs: document codebase

--- leech/src/modules/bruteforce_subdomains/mod.rs
+++ leech/src/modules/bruteforce_subdomains/mod.rs
@@ -6,7 +6,22 @@ use std::fs;
 use std::path::PathBuf;
 use std::time::Duration;
 
+
+/// The settings to configure a subdomain bruteforce
+#[derive(Debug)]
 pub struct BruteforceSubdomainsSettings {
+    /// The domain to use as base name. It shouldn't end in a . like DNS names.
     pub domain: String,
+    /// Path to a wordlist that can be used for subdomain enumeration.
+    ///
+    /// The entries in the wordlist are assumed to be line seperated.
     pub wordlist_path: PathBuf,
+}
+
+/// Enumerates subdomains by brute forcing dns records with a wordlist.
+///
+/// **Parameter**:
+/// - `settings`: [BruteforceSubdomainsSettings]
+pub async fn bruteforce_subdomains(settings: BruteforceSubdomainsSettings) -> Result<(), String> {
+    unimplemented!()
 }
