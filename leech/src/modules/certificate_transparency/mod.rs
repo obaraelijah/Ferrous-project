90f1910c4a6f35047cca16ac106101b0a7642f28
obaraelijah <elijahobara357@gmail.com>
2024-08-28 17:27:52 +0300

    feat: crt.sh entry

--- leech/src/modules/certificate_transparency/mod.rs
+++ leech/src/modules/certificate_transparency/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! For technical information, see [here](https://certificate.transparency.dev/)
 
+pub mod crt_sh_types;
+
 pub struct CertificateTransparencySettings {
     pub target: String,
     pub include_expired: bool,
