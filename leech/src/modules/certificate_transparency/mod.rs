690272bc80f6d85bfa3a873c07edf82ce0d987cb
obaraelijah <elijahobara357@gmail.com>
2024-08-28 17:31:52 +0300

    feat: Query the crt.sh certificate transparency api

--- leech/src/modules/certificate_transparency/mod.rs
+++ leech/src/modules/certificate_transparency/mod.rs
@@ -12,3 +12,43 @@ pub struct CertificateTransparencySettings {
     /// Also include already expired certificates
     pub include_expired: bool,
 }
+
+/// Query the crt.sh certificate transparency api.
+///
+/// **Parameters**:
+/// - `settings`: [CertificateTransparencySettings]
+pub async fn query_ct_db(settings: CertificateTransparencySettings) {
+    let mut db_config = DatabaseConfiguration::new(DatabaseDriver::Postgres {
+        name: "certwatch".to_string(),
+        host: "crt.sh".to_string(),
+        port: 5432,
+        user: "guest".to_string(),
+        password: "".to_string(),
+    });
+    db_config.disable_logging = Some(true);
+    let db = Database::connect(db_config).await.unwrap();
+
+    let rows = match db
+        .raw_sql(
+            get_query(&settings.target, settings.include_expired).as_str(),
+            None,
+            None,
+        )
+        .await
+    {
+        Ok(rows) => rows,
+        Err(err) => {
+            println!("Error querying data: {err}");
+            return;
+        }
+    };
+
+    let entries: Vec<String> = rows
+        .into_iter()
+        .map(|row| row.get(2).unwrap())
+        .sorted()
+        .dedup()
+        .collect();
+
+    println!("{entries:#?}");
+}
