5c8b07f335a4476ab735a5d3d78976f2b7bfa14f
obaraelijah <elijahobara357@gmail.com>
2024-08-28 17:30:09 +0300

    docs: document codebase

--- leech/src/modules/certificate_transparency/mod.rs
+++ leech/src/modules/certificate_transparency/mod.rs
@@ -5,7 +5,10 @@
 pub mod crt_sh_db;
 pub mod crt_sh_types;
 
+/// Settings for a certificate transparency search request
 pub struct CertificateTransparencySettings {
+    /// The target domain to query
     pub target: String,
+    /// Also include already expired certificates
     pub include_expired: bool,
 }
