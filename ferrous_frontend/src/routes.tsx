9d705602a80bc710618bb3f63d4689b952ac6ab8
obaraelijah <elijahobara357@gmail.com>
2024-09-03 08:07:51 +0300

    feat: Ported leech management from old ferrous

--- ferrous_frontend/src/routes.tsx
+++ ferrous_frontend/src/routes.tsx
@@ -1,6 +1,6 @@
 import Home from "./views/home";
-import React from "react";
 import { Router } from "./utils/router";
+import FerrousNetwork from "./views/ferrous-network";
 import Me from "./views/me";
 import Login from "./views/login";
 
@@ -10,6 +10,7 @@ export const ROUTES = {
     LOGIN: ROUTER.add({ url: "login", parser: {}, render: () => <Login /> }),
     HOME: ROUTER.add({ url: "", parser: {}, render: () => <Home /> }),
     ME: ROUTER.add({ url: "me", parser: {}, render: () => <Me /> }),
+    FERROUS_NETWORK: ROUTER.add({ url: "ferrous-network", parser: {}, render: () => <FerrousNetwork /> }),
 };
 
 ROUTER.finish();
