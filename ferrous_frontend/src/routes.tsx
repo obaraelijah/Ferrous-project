f60f4036b3fa000e33637e2922d7920a721c1379
obaraelijah <elijahobara357@gmail.com>
2024-09-03 08:38:54 +0300

    feat: Add frontend for /api/v1/admin/users

--- ferrous_frontend/src/routes.tsx
+++ ferrous_frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { Router } from "./utils/router";
 import FerrousNetwork from "./views/ferrous-network";
 import Me from "./views/me";
 import Login from "./views/login";
+import UserManagement from "./views/admin/user-management";
 
 export const ROUTER = new Router();
 
@@ -11,6 +12,8 @@ export const ROUTES = {
     HOME: ROUTER.add({ url: "", parser: {}, render: () => <Home /> }),
     ME: ROUTER.add({ url: "me", parser: {}, render: () => <Me /> }),
     FERROUS_NETWORK: ROUTER.add({ url: "ferrous-network", parser: {}, render: () => <FerrousNetwork /> }),
+
+    ADMIN_USER_MANAGEMENT: ROUTER.add({ url: "admin/users", parser: {}, render: () => <UserManagement /> }),
 };
 
 ROUTER.finish();
