0eb285e4be5f0dc3a114de09311a30148e8d3c96
obaraelijah <elijahobara357@gmail.com>
2024-09-04 12:51:51 +0300

    feat: Added pep-8

--- ferrous_frontend/scripts/gen-api.py
+++ ferrous_frontend/scripts/gen-api.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+
 import os
 import pathlib
 
-script_file = pathlib.Path(__file__)
-project_root = script_file.parent.parent
-spec = project_root / "openapi.json"
-generated = project_root / "src" / "api" / "generated"
-config = generated / "config.json"
+def main():
+    script_file = pathlib.Path(__file__)
+    project_root = script_file.parent.parent
+    spec = project_root / "openapi.json"
+    generated = project_root / "src" / "api" / "generated"
+    config = generated / "config.json"
 
-command = f"npx @openapitools/openapi-generator-cli generate -g typescript-fetch -i {spec} -o {generated} -c {config}"
-print(command)
-os.system(command)
+    command = f"npx @openapitools/openapi-generator-cli generate -g typescript-fetch -i {spec} -o {generated} -c {config}"
+    print(command)
+    os.system(command)
 
-print("Patch PortOrRange.ts")
-with open(generated / "models" / "PortOrRange.ts", "w") as f:
-    f.write("""
+    print("Patch PortOrRange.ts")
+
+    with open(generated / "models" / "PortOrRange.ts", "w") as f:
+        f.write("""
 export type PortOrRange = number | string;
 export function PortOrRangeFromJSON(json: any): PortOrRange {
     return PortOrRangeFromJSONTyped(json, false);
@@ -30,3 +33,7 @@ export function PortOrRangeToJSON(value?: PortOrRange | null): any {
     return value;
 }
 """)
+
+
+if __name__ == '__main__':
+    main()
