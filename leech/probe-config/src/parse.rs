ad1ee5dfeac2a405f4852c3bec69b923872dbda3
obaraelijah <elijahobara357@gmail.com>
2024-10-11 01:27:30 +0300

    feat:  Error handling

--- leech/probe-config/src/parse.rs
+++ leech/probe-config/src/parse.rs
@@ -41,10 +41,33 @@ pub enum Prevalence {
 
 #[derive(Debug)]
 pub enum ParseError {
+    /// The initial `service: <name>` line is missing
     MissingService,
+    /// The second line `prevalence:` is missing
     MissingPrevalence,
+    /// The third line `probes:` is missing or has no lines following
     MissingProbes,
+    
+    /// A probe's value has been passed twice
+    DuplicateValue(&'static str, usize),
+    /// A probe's value is missing
+    MissingValue(&'static str, usize),
+    /// An unknown probe
+    UnknownValue(usize),
+    
+    /// Both `payload_str` and `payload_b64` are specified
+    ConflictingPayload { probe_line: usize },
+    
+    /// The sub regex must be the last key in any probe
+    ValueAfterSubRegex(usize),
+    /// A sub regex item before `sub_regex:`
+    UnexpectedSubRegex(usize),
+    /// The sub regex is specified but empty,
+    MissingSubRegex { probe_line: usize },
+    
+    /// Invalid value for `protocol: `
     InvalidProtocol(usize),
+    /// Invalid value for `prevalence: `
     InvalidPrevalence(usize),
 }
 
