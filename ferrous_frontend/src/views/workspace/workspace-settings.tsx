41b099ae12e8f0f9e356cbce75a4276fb7395b3b
obaraelijah <elijahobara357@gmail.com>
2024-10-19 10:37:28 +0300

    feat: Upstream

--- ferrous_frontend/src/views/workspace/workspace-settings.tsx
+++ ferrous_frontend/src/views/workspace/workspace-settings.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { FullWorkspace } from "../../api/generated";
 
-type WorkspaceSettingsProps = {};
+type WorkspaceSettingsProps = {
+    workspace: FullWorkspace;
+};
 type WorkspaceSettingsState = {};
 
 export default class WorkspaceSettings extends React.Component<WorkspaceSettingsProps, WorkspaceSettingsState> {
     constructor(props: WorkspaceSettingsProps) {
         super(props);
+
         this.state = {};
     }
+
     render() {
-        return <div></div>;
+        return (
+            <div className={"pane workspace-settings-container"}>
+                <h1 className={"heading"}> Workspace Settings </h1>
+
+                <div className="pane"></div>
+            </div>
+        );
     }
 }
